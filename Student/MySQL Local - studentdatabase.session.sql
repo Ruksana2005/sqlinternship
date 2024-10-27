43d627d36f9868226e67d456fa916cfaba055bc7
Ruksana2005 <143307977+Ruksana2005@users.noreply.github.com>
2024-10-27 14:23:35 +0530

    Add files via upload

---
+++ Student/MySQL Local - studentdatabase.session.sql
@@ -0,0 +1,9 @@
+INSERT INTO Results (exam_id, student_id, marks_obtained, grade)
+VALUES (6, 6, 95, 'A');
+
+INSERT INTO students (first_name, last_name, date_of_birth, gender, email, phone_number, address, admission_date, department_id)
+VALUES ('Alice', 'Miller', '2003-04-12', 'F', 'alice.miller@example.com', '1231231234', '789 Maple St, City, State', '2024-01-15', 2);
+select * from students;
+INSERT INTO students (first_name, last_name, date_of_birth, gender, email, phone_number, address, admission_date, department_id)
+VALUES ('Rehana', 'Shaik', '2003-04-12', 'F', 'Rehana@gmail.com', '1231231234', '789 Maple St, City, State', '2024-01-15', 2);
+select * from students;
